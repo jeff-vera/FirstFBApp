cc7dcb8d16c9774328028dcaa42e8e5b9c5636b4
Jeff Vera <jeff.vera@gmail.com>
2012-03-28 21:05:55 -0400

    - Wow, it ran my tests and so the build failed... fixing so build passes.

--- FirstFBApp/Controllers/HomeController.cs
+++ FirstFBApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace FirstFBApp.Controllers
 	{
 		public ActionResult Index()
 		{
-			ViewBag.Message = "Welcome to ASP.NET MVC!!";
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
 
 			return View();
 		}
